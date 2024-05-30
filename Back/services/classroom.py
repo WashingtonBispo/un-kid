35f1503940a892440d6c692eb3afb5e14d633b31
WashingtonBispo <juniorbispo515@gmail.com>
2024-05-30 13:50:29 -0300

    Adicionando nova maneira de dar join com a tabala 'Account'

--- Back/services/classroom.py
+++ Back/services/classroom.py
@@ -1,16 +1,18 @@
 from .context import engine
 from ..domain.classroom import Classroom
 from ..domain.account import Account
+from ..domain.transaction import Transaction
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from fastapi import HTTPException
+from sqlalchemy.orm import joinedload
 import json
 import uuid
 
 def post(json):
     with Session(engine) as session:
         classroom = Classroom(
-            id=str(uuid.uuid4())[:6],
+            id=str(uuid.uuid4())[:6].upper(),
             name=json.name,
             image=json.image
         )
@@ -26,7 +28,7 @@ def getAll():
 
 def get(code):
     with Session(engine) as session:
-        query = select(Classroom).where(Classroom.id==code).join(Account)
+        query = select(Classroom).where(Classroom.id==code).options(joinedload(Classroom.accounts))
         result = session.scalar(query)
         if(result==None):
             raise HTTPException(status_code=404, detail="Não foi possível encontrar uma sala")
