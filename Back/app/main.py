b31af1608a91b1099b6b1e49af99407f1eb1e5c1
WashingtonBispo <juniorbispo515@gmail.com>
2024-06-01 21:48:01 -0300

    Adicionando Middleware

--- Back/app/main.py
+++ Back/app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 
 from .routers import classroom, account, transaction
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(swagger_ui_parameters={"deepLinking": False})
 
-app.include_router(classroom.router)
-app.include_router(account.router)
-app.include_router(transaction.router)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(classroom.router, prefix="/api")
+app.include_router(account.router, prefix="/api")
+app.include_router(transaction.router, prefix="/api")
 
 
 @app.get("/")
